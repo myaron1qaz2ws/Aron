6b7d0d1f387b20fe9513a97ba8d2d4ba20e9b503
myaron1qaz2ws <myaron1qaz2ws@gmail.com>
2020-04-03 21:26:26 +0800

    first

---
+++ stack.py
@@ -0,0 +1,24 @@
+# Script Name       : stack.py
+
+class Stack():
+    ''' 定义 stack 类 '''
+    def __init__(self):
+        self.items = []         #初始化空栈，栈使用列表表示
+
+    def push(self,items):
+        self.items.append(items)        #将items压栈
+
+    def pop(self):
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            raise Exception("Stack is empty!")
+
+    def isEmpty(self):
+        return self.items == []
+    
+    def peek(self):
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
